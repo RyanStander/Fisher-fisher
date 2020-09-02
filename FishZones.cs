7eaeda8d2680ff173c70381adfc6b02023d5dbce
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-02 12:37:02 +0200

    AI movement and Fish zone
    Scripts used to control the fishing spots and the Enemy movement

---
+++ FishZones.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishZones : MonoBehaviour
+{
+    //the max fish amount
+    [SerializeField] float fishStock=10;
+    //the current fish amount
+    private float _curFishStock;
+    //the speed at which fish restock
+    [SerializeField] float fishRestockSpeed=3;
+    //the amount of fish that comes back over time
+    [SerializeField] float fishRestockAmount = 1;
+    // Start is called before the first frame update
+    private float nextDeplete;
+    void Start()
+    {
+        _curFishStock = fishStock;
+
+        InvokeRepeating("Regenerate", fishRestockSpeed, fishRestockSpeed);
+    }
+    //regenerating the fish amount so that the supply can come back
+    private void Regenerate()
+    {
+        //if missing fish they will repopulate over time
+        if (_curFishStock < fishStock)
+            _curFishStock += fishRestockAmount;
+
+        if (_curFishStock > fishStock)
+            _curFishStock = fishStock;
+    }
+    public void DepleteFishStock(float depleteRate)
+    {
+        //when a fishing boat is nearby they will deplete the amount of fish in a spot over time
+        if (Time.time > nextDeplete)
+        {
+            nextDeplete = Time.time + depleteRate;
+
+            _curFishStock--;
+
+            //Debug.Log("Fish are being depleted:" + _curFishStock);
+        }
+        if (_curFishStock < 1)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
