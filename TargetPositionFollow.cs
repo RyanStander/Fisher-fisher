80066d8848c743e3bd01006137c90c98f2117d6b
JacquesAV <JacquesAV@users.noreply.github.com>
2020-09-02 12:44:12 +0200

    Updated TargetPositionFollow
    Removes easing code as it is not needed.

--- TargetPositionFollow.cs
+++ TargetPositionFollow.cs
@@ -6,21 +6,8 @@ public class TargetPositionFollow : MonoBehaviour
 {
     public GameObject targetObject;
 
-    [SerializeField]
-    private float easingSpeed=0.9f;
-
     void Update()
     {
         transform.position = targetObject.transform.position;
-        //easingTowards();
-    }
-
-    private void easingTowards()
-    {
-        //Calculate a step for the following object towards the target object
-        float step = easingSpeed * Time.deltaTime;
-
-        //Move towards the target using the step
-        transform.position = Vector3.MoveTowards(transform.position, targetObject.transform.position, step);
     }
 }
