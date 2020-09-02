8c68fd25145f57072ce6dff3ca310b60d601ec15
JacquesAV <JacquesAV@users.noreply.github.com>
2020-09-02 09:17:49 +0200

    Camera Follow Code
    Player Camera looks at the given target (player in this case), meanwhile Target Position Follow is for the empty object that contains the camera, which constantly stays at the boat coordinates. This prevents the camera rotating when the boat rotates.

---
+++ PlayerCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float cameraSpeed=20; //Rotation speed for camera
+    
+    public GameObject target;
+
+    void Start()
+    {
+        //Mouse Locking
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotates the camera to constantly look at where the boat is
+        transform.LookAt(target.transform);
+
+        CameraShift();
+        //KeyCameraShift();
+    }
+
+    //If rotating using mouse
+    private void CameraShift()
+    {
+        //Gets the mouse axis using their reference axisName
+        float horizontal = cameraSpeed * Input.GetAxis("Mouse X");
+
+        //Rotates the camera based on the mouse position
+        transform.Translate(Vector3.left * Time.deltaTime * horizontal);
+    }
+
+    //If rotating using keys
+    private void KeyCameraShift()
+    {
+        //Right rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * cameraSpeed);
+        }
+        //Left rotation
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * cameraSpeed);
+        }
+    }
+}
---
+++ TargetPositionFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPositionFollow : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    [SerializeField]
+    private float easingSpeed=0.9f;
+
+    void LateUpdate()
+    {
+        transform.position = targetObject.transform.position;
+        //easingTowards();
+    }
+    private void easingTowards()
+    {
+        //Calculate a step for the following object towards the target object
+        float step = easingSpeed * Time.deltaTime;
+
+        //Move towards the target using the step
+        transform.position = Vector3.MoveTowards(transform.position, targetObject.transform.position, step);
+    }
+}
