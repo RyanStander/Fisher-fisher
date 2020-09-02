5857de2beda47bb4baf72b080a29ad56474f5475
JacquesAV <JacquesAV@users.noreply.github.com>
2020-09-02 16:57:53 +0200

    Updated Boat Movement (Input)
    Movement updated to function using input axis controls rather than key controls. Movement also improved to act more like a boat or "tank", only rotating when enough speed has been built up.

--- BoatMovement.cs
+++ BoatMovement.cs
@@ -6,9 +6,13 @@ public class BoatMovement : MonoBehaviour
 {
     [SerializeField]
     [Header("Boat Attributes")]
-    public float moveSpeed; //Speed of the boat
-    public float turnSpeed; //Speed at which the boat can turn
-    public float dragFactor; //Factor at which the velocity is reduced
+    public float moveSpeed=20; //Speed of the boat
+    [Range(1,5)]
+    public float turnStrength=3; //Multiplier at which the boat can turn when moving
+
+    public float dragFactor = 0.9f; //Factor at which the velocity is reduced
+
+    private float moveForce;
 
     private Rigidbody rBody;
 
@@ -19,29 +23,33 @@ public class BoatMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
-        //Forward movement
-        if(Input.GetKey(KeyCode.W))
-        {
-            rBody.AddRelativeForce(Vector3.forward * moveSpeed);
-        }
-        //Backward movement
-        else if (Input.GetKey(KeyCode.S))
-        {
-            rBody.AddRelativeForce(-Vector3.forward/2* moveSpeed);
-        }
+        axisInputMovement();
+    }
 
-        //Right rotation
-        if (Input.GetKey(KeyCode.D))
+    //Movement based on input of the axis controls
+    private void axisInputMovement()
+    {
+        //Axis Input
+        moveForce = Input.GetAxis("Vertical") * moveSpeed;
+
+        //If input is negative (reversing), half force
+        if (moveForce < 0)
         {
-            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+            moveForce *= 0.75f;
         }
-        //Left rotation
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(-Vector3.up * turnSpeed * Time.deltaTime);
-        }
+        
+        //Forward/Backwards movement based on axis input
+        rBody.AddForce(transform.forward * moveForce);
+
+        //Normalized for the inputs
+        Vector2 horizontalInputVector = new Vector2(Input.GetAxis("Horizontal"),0);
+
+        //Rotate based on input and current player movement forward
+        transform.Rotate(Vector3.up * horizontalInputVector.normalized.x * turnStrength * moveForce * Time.deltaTime);
+        //transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime);
 
         //Reduces the velocity over time to slow down the movement
         rBody.velocity *= dragFactor;
     }
+
 }
