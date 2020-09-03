448bd51a8dabc4809672d46610d9cbbf3d8976e4
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-03 10:36:53 +0200

    Multiple scripts
    Updated Movement and fish zones and added enemy counter, fish counter and main menu

---
+++ FishCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FishCounter : MonoBehaviour
+{
+    private TextMeshProUGUI _fishLeft;
+    private List<FishZones> _fishZones=new List<FishZones>();
+    private float _maxFish;
+    private float _curFish;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] go = GameObject.FindGameObjectsWithTag("FishingZone");
+        foreach (GameObject gameObject in go)
+        {
+            _fishZones.Add(gameObject.GetComponent<FishZones>());
+        }
+        _fishLeft = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _maxFish = 0;
+        _curFish = 0;
+        foreach (FishZones fishZone in _fishZones)
+        {
+            _maxFish+=fishZone.GetMaxFishStock();
+            _curFish+=fishZone.GetCurFishStock();
+        }
+        _fishLeft.SetText(_curFish+"/"+_maxFish);
+    }
+}
