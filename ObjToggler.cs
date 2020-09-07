aff0ba68e8b038af38da2491b4daca86c5bf4340
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-07 12:34:32 +0200

    Added a toggler
    Either turns on or off a ui element

---
+++ ObjToggler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ObjToggler : MonoBehaviour
+{
+    [SerializeField] GameObject toggleObjOn=null;
+    [SerializeField] GameObject toggleObjOff=null;
+
+    public void ToggleObj()
+    {
+        if (toggleObjOn != null)
+            toggleObjOn.SetActive(true);
+        if (toggleObjOff != null)
+            toggleObjOff.SetActive(false);
+    }
+}
