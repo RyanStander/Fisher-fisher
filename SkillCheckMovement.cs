4bf656bb2ddda9378d41bde08ae5563930d87df7
JacquesAV <JacquesAV@users.noreply.github.com>
2020-09-07 09:21:29 +0200

    Created Skill Check Scrips (First iteration)
    First iteration of the skill check where the bars will move between the contained area.

---
+++ SkillCheckMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillCheckMovement : MonoBehaviour
+{
+    [Range (1,5)]
+    public int skillBarSpeed = 2;
+    private float rightBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Autocalculates offset for the bar
+        float borderOffset = GetComponent<RectTransform>().rect.width/1.75f;
+
+        //Calculated the right boundary of the containing parent object
+        rightBound = transform.parent.GetComponent<RectTransform>().rect.width / 2 - borderOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves the bar
+        transform.Translate(Vector3.right * skillBarSpeed);
+
+        //Checks that bar is within the boundaries
+        if (transform.localPosition.x <= -rightBound || transform.localPosition.x >= rightBound)
+        {
+            //Reverses the speed
+            skillBarSpeed *= -1;
+        }
+    }
+}
