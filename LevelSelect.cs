96ba2fb2dd197d94ac59e53a9b48232cf93da1ba
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-07 12:33:50 +0200

    Added a level selector
    Click on another level to change the shown one

---
+++ LevelSelect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class LevelSelect : MonoBehaviour
+{
+    private int selectedLevel = 0;
+    [SerializeField] string[] _level=null;
+    [SerializeField] TextMeshProUGUI _levelText=null;
+    public void Start()
+    {
+        _levelText.SetText("Level: " + _level[selectedLevel]);
+    }
+    public void PlayGame()
+    {
+        //Loads the selected level        
+
+        SceneManager.LoadScene(_level[0]);
+    }
+    public void SelectLeft()
+    {
+        if (selectedLevel == 0)
+            selectedLevel = _level.Length - 1;
+        else
+            selectedLevel--;
+        _levelText.SetText("Level: " + _level[selectedLevel]);
+    }
+    public void SelectRight()
+    {
+        if (selectedLevel == _level.Length - 1)
+            selectedLevel = 0;
+        else
+            selectedLevel++;
+        _levelText.SetText("Level: " + _level[selectedLevel]);
+    }
+
+}
