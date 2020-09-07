9fd210fea06894f9982c2a2d82f89d1188d76f3f
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-07 12:33:20 +0200

    Added difficulty Script
    Changes the selected difficulty

---
+++ DifficultySelection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySelection : MonoBehaviour
+{
+    [SerializeField] GameObject mainButton=null;
+    [SerializeField] GameObject[] otherButtons= new GameObject[2];
+    public void ChooseButton()
+    {
+        mainButton.transform.localScale = new Vector3(1,1);
+        for (int i = 0; i < otherButtons.Length; i++)
+        {
+            otherButtons[i].transform.localScale = new Vector3(0.75f, 0.75f);
+        }
+    }
+}
