fc7f07157b322ec5caa3f9a8f108292470ce4188
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-08 16:42:05 +0200

    Create TotalCoinsGainedText.cs
    Shows all your money gained

---
+++ TotalCoinsGainedText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TotalCoinsGainedText : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _totalText=null;
+    public void Start()
+    {
+        _totalText.SetText("Total coins: " + StaticValues.Coins);
+    }
+}
