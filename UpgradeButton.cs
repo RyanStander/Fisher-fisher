9d64f0a142b8011b5a1cd62bd3ac6bbbdcdd2d78
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-08 16:42:18 +0200

    Create UpgradeButton.cs
    Used for upgrading ship

---
+++ UpgradeButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeButton : MonoBehaviour
+{
+    [SerializeField] Sprite[] upgradeSprite = null;
+    private int _currImg;
+    [SerializeField] int[] upgradeCost=null;
+    [SerializeField] string UpgradeType=null;
+    public void UpgradeSprite()
+    {
+        if (_currImg != upgradeSprite.Length-1&& StaticValues.Coins>=upgradeCost[_currImg])
+        {
+            StaticValues.Coins -= upgradeCost[_currImg];
+            _currImg++;
+            gameObject.GetComponent<Image>().sprite = upgradeSprite[_currImg];
+            Debug.Log("Subtracted " + upgradeCost[_currImg-1] + " and " + StaticValues.Coins + " coins remaining");
+            switch (UpgradeType)
+            {
+                case "plunger":
+                    StaticValues.PlungerStrength = true;
+                    Debug.Log("Plunger Strength has been unlocked");
+                    break;
+                case "currency":
+                    StaticValues.CurrencyUpgrade++;
+                    Debug.Log("Money Maker make more money magnitude: " + StaticValues.CurrencyUpgrade);
+                    break;
+                case "engine":
+                    StaticValues.EnginePower++;
+                    Debug.Log("Enigmatic engine egnition escalated: " + StaticValues.EnginePower);
+                    break;
+            }
+        }
+    }
+}
