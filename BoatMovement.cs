a12f604bd69aab71265b1b7be8e8c8dbc820b7b1
JacquesAV <JacquesAV@users.noreply.github.com>
2020-09-08 16:57:29 +0200

    Updated BoatMovement for Boost
    Boat now has functionality for a customizable speed boost from the inspector.

--- BoatMovement.cs
+++ BoatMovement.cs
@@ -15,21 +15,52 @@ public class BoatMovement : MonoBehaviour
     public float turningStrength = 0.05f;
     [Range(0.5f, 0.99f)]
     public float playerSpeedSlowdown = 0.8f;
-    [SerializeField] float[] speedBoostLevel = {0,0.05f,0.10f,0.15f,0.20f };
+    [SerializeField] float[] enginePowerLevel = {0,0.05f,0.10f,0.15f,0.20f };
+    private float playerSpeed;
+
+    [Header("Accelaration Boost Attributes")]
+    //Accelaration boost
+    [Range(0.5f, 5f)]
+    public float maximumAccelerationBoost = 5f;
+    [Range(0.005f, 0.05f)]
+    public float accelarationBoostRate = 0.05f;
+    [Range(1f, 5f)]
+    public float boostTime = 5;
+    private float boostTimeRemaining;
+    private float currentAccelarationBoost = 0;
+    private bool isAccelarationBoosting = false;
 
     private Rigidbody rBody;
 
+    void OnEnable()
+    {
+        EventManager.onCriticalSkillCheck += enableAccelarationBoost;
+    }
+    void OnDisable()
+    {
+        EventManager.onCriticalSkillCheck -= enableAccelarationBoost;
+    }
+    private void enableAccelarationBoost()
+    {
+        isAccelarationBoosting = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        boostTimeRemaining = boostTime;
         rBody = GetComponent<Rigidbody>();
     }
     void Update()
     {
+        //Boosting
+        if (isAccelarationBoosting)
+        {
+            accelarationBoost();
+        }
+        Debug.Log(currentAccelarationBoost);
         playerMovement();
     }
- 
-    private float playerSpeed;
+
     private void playerMovement()
     {
         //Turning
@@ -43,13 +74,13 @@ public class BoatMovement : MonoBehaviour
         }
 
         //Forward/Backwards
-        if (Input.GetKey(KeyCode.W) && playerSpeed <= maximumSpeed)
+        if (Input.GetKey(KeyCode.W) && playerSpeed <= maximumSpeed+currentAccelarationBoost)
         {
-            playerSpeed += speedFrontStrength+speedBoostLevel[StaticValues.EnginePower];
+            playerSpeed += speedFrontStrength+enginePowerLevel[StaticValues.EnginePower]+ currentAccelarationBoost;
         }
-        if (Input.GetKey(KeyCode.S) && playerSpeed >= -maximumSpeed)
+        if (Input.GetKey(KeyCode.S) && playerSpeed >= -maximumSpeed + currentAccelarationBoost) //Speed up is weaker in reverse
         {
-            playerSpeed -= speedBackStrength - speedBoostLevel[StaticValues.EnginePower]; //Speed up is weaker in reverse
+            playerSpeed -= speedBackStrength - enginePowerLevel[StaticValues.EnginePower]+ currentAccelarationBoost;
         }
         
         //Apply the speed to the player
@@ -57,5 +88,26 @@ public class BoatMovement : MonoBehaviour
 
         //Apply a slowdown to the player speed
         playerSpeed *= playerSpeedSlowdown;
+    }
+
+    private void accelarationBoost()
+    {
+        //Reduce time remaining on boost
+        boostTimeRemaining -= Time.deltaTime;
+
+        //Speed up until hitting maximum boost
+        if (currentAccelarationBoost<maximumAccelerationBoost)
+        {
+            //Apply boost to current accelaration boost
+            currentAccelarationBoost += accelarationBoostRate;
+        }
+        else if (boostTimeRemaining <= 0)
+        {
+            //Reset and end boost
+            boostTimeRemaining = boostTime;
+            currentAccelarationBoost = 0;
+            isAccelarationBoosting = false;
+            return;
+        }
     }
 }
