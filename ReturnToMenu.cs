1cf679c695dbbd7a2a48a6d2d6ac2b5f66db835f
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-08 16:40:44 +0200

    Create ReturnToMenu.cs
    Returns back to the menu screen

---
+++ ReturnToMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ToMenu()
+    {
+        //Loads the selected level        
+
+        SceneManager.LoadScene("StartScene");
+    }
+}
