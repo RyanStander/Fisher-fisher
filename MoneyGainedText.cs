6f8836ddff1312a9ccc40dcb449dbfe265feccb2
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-08 16:40:21 +0200

    Create MoneyGainedText.cs
    Shows how much money you make

---
+++ MoneyGainedText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyGainedText : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _MoneyText=null;
+    public void Start()
+    {
+        _MoneyText.SetText("Coins gained: " +StaticValues.CoinsGained);
+    }
+}
