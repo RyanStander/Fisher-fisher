ace3b4405ed33f696570d9725a127fc94c78bf8e
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-08 11:24:44 +0200

    Created StaticValuesClass
    Yo this pretty cool

---
+++ StaticValues.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticValues : MonoBehaviour
+{
+    static public bool PlungerStrength = false;
+
+    static public int EnginePower = 0;
+
+    static public int CurrencyUpgrade=0;
+
+    static public int Coins = 2000;
+}
