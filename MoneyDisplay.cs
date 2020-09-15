244b276b12c8b5bb873538cd7df1c685b66cc984
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-09-15 11:28:55 +0200

    Create MoneyDisplay.cs

---
+++ MoneyDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyDisplay : MonoBehaviour
+{
+    private TextMeshProUGUI _money;
+    void Start()
+    {
+        _money = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _money.SetText(StaticValues.Coins.ToString());
+    }
+}
